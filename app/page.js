4bc0c20c5ddd867270bfce3c2b44218efd4350e8
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-06 21:18:37 +0330

    Navigating between pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
-  return <h1>Our home page of Stelix website</h1>;
+  return (
+    <div>
+      <Navigation />
+
+      <h1>Our home page of Stelix website</h1>
+    </div>
+  );
 }
