2847e8953899209c80b6e444349517a426273b93
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-06 21:43:46 +0330

    creating a layout

--- app/page.js
+++ app/page.js
@@ -1,12 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import Navigation from "./components/Navigation";
-
 export default function Home() {
   return (
     <div>
-      <Navigation />
-
       <h1>Our home page of Stelix website</h1>
     </div>
   );
