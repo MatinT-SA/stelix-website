81a2a4ac1bbae53d688630d0fd9d4b64d036912b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-29 19:28:12 +0330

    improving our navigation

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -24,14 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`}
+        className={`bg-primary-950 text-primary-100 min-h-screen antialiased flex flex-col ${josefin.className}`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Stelix</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
