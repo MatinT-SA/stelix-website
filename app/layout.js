731a6ecb0e7a8093f01b236005564996080ec1c3
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-05-29 18:40:13 +0330

    adding metadata

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+import "@/app/_styles/globals.css";
+
 export const metadata = {
-  title: "Stelix website",
+  title: {
+    template: "%s: Stelix",
+    default: "Welcome Stelix",
+  },
+
+  description:
+    "Escape to Stelix — browse cozy cabins, book unforgettable stays, and let your next adventure begin in a world where comfort meets wonder.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
