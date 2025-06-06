ca6cba9d54023361f3a3ab07cf1e145a1dfb1ba9
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-06-06 15:10:55 +0330

    implementing the logic for filter on the client side

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600;
 
@@ -24,6 +25,10 @@ export default function Page({ searchParams }) {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
 
       <Suspense fallback={<Spinner />}>
         <CabinList filter={filter} />
