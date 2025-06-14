599804047ada1e9566888e0936993616b3952531
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-06-14 15:00:00 +0330

    setting up next auth

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import Image1 from "@/public/about-1.jpg";
-import Image2 from "@/public/about-2.jpg";
+import Image from "next/image";
 import { getCabins } from "../_lib/data-service";
 
 export const revalidate = 86400;
