a516a250a4d997f918da338d28b45869127e6d70
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-07-10 20:24:42 +0330

    creating our first reservation/booking

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -29,7 +29,24 @@ export async function updateGuest(formData) {
 }
 
 export async function createBooking(bookingData, formData) {
-  console.log(formData);
+  const session = await auth();
+  if (!session) throw new Error("You must be loggied in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) throw new Error("Booking could not be created");
 }
 
 export async function deleteBooking(bookingId) {
