b328979feb08fb3a058396f1b993ca45aad44614
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2025-07-27 00:07:21 +0330

    responsiveness - all screens for account page

--- app/account/layout.js
+++ app/account/layout.js
@@ -2,7 +2,7 @@ import SideNavigation from "@/app/_components/SideNavigation";
 
 export default function Layout({ children }) {
   return (
-    <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+    <div className="grid sm:grid-cols-[16rem_1fr] grid-cols-[12rem_1fr] h-full xs:gap-12 gap-2">
       <SideNavigation />
       <div className="py-1">{children}</div>
     </div>
